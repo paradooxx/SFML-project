ec2f5d795f3cbd2dc7039a810db099b9241b1aeb
theParas <55276059+theParas@users.noreply.github.com>
2020-11-18 13:39:43 +0545

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,39 +1,22 @@
-#include "Player.h"
-Player::Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed) :
-	animate(texture, imageCount, switchTime)
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "animation.h"
+class Player
 {
-	this->speed = speed;
-	row = 0;
-	faceRight = true;
+public:
+	Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
+	//~Player(); // not necessary
 
-	body.setSize(sf::Vector2f(100.f, 200.f));
-	body.setPosition({ 300.f,300.f });
-	body.setOrigin(body.getSize() / 2.f);
-	body.setTexture(texture);
-}
-void Player::update(float deltaTime)
-{
-	sf::Vector2f movement(0.f, 0.f);
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		movement.x -= speed * deltaTime;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		movement.x += speed * deltaTime;
-	if (movement.x == 0.f)
-		row = 0;
-	else
-	{
-		row = 1;
-		if (movement.x > 0.f)
-			faceRight = true;
-		else
-			faceRight = false;
-	}
-	animate.update(row, deltaTime, faceRight);
-	body.setTextureRect(animate.uvRect);
-	body.move(movement);
-}
+	void update(float deltaTime);
+	void Draw(sf::RenderWindow& window);
 
-void Player::Draw(sf::RenderWindow& window)
-{
-	window.draw(body);
-}
+	sf::Vector2f GetPosition() { return body.getPosition(); }
+
+private:
+	sf::RectangleShape body; 
+	animation animate;
+	unsigned int row;
+	float speed;
+	bool faceRight;
+};
+
